50baa958f7e59f5f2837ec2b65b89401ef5e0574
Villa <venkatesh.villa_HCL@Kantar.com>
2022-03-29 20:01:48 +0530

    second commit

--- src/test/java/com/post/Stripe_post.java
+++ src/test/java/com/post/Stripe_post.java
@@ -41,4 +41,22 @@ public class Stripe_post {
 		int statusCode=response.getStatusCode();
 		System.out.println(statusCode);  
 	}
+@Test()
+	  public void f2() {
+		baseURI="https://api.stripe.com";
+		//created password for this is Stripe@pi6730! and email is villa.venkatesh@gmail.com
+		
+		String body= "{\"email\":\"random4.random@gmail.com\",\"name\":\"random4\",\"phone\":\"7012342394\",\"description\":\"add 4 random\"}";
+		
+		
+		Response response= given().header("Authorization","Bearer sk_test_51KhwjVSBBYhkKf2KqvUfL61i8djb5b4q19JQ7vUGhzX6IyBE5yDiRWwrMfcbFtXD2LU6avUHiy3zAzhKDRqSeMfl002ElJY8x4").
+				formParam("email","random5.random@gmail.com").formParam("name","random5").formParam("phone","7012342395").formParam("description","add 5 random").post("/v1/customers");
+		System.out.println("------------------------specific customer--------------------------");
+		
+		response.prettyPrint();
+		String res=response.asString();
+		
+		int statusCode=response.getStatusCode();
+		System.out.println(statusCode);  
+	}
 }
